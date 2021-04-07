4142f028b62ef8bee4061f13fd3b45750e0571cb
Neeraj Kumar Chauhan <kneeraj460@gmail.com>
2021-04-07 10:30:28 +0530

    challange

---
+++ React/challenge1/src/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Neeraj Netflix Favourites</h1>
+      <p>These are my favourite netflix series : </p>
+      <ol>
+        <li>Scared Games</li>
+        <li>Delhi Crime</li>
+        <li>She</li>
+        <li>Breathe</li>
+        <li>Leila</li>
+      </ol>
+    </div>
+  );
+}
+
+export default App;
